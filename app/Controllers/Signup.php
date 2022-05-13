efb19745ffa3c59f3b758aee924963711c1a5ef6
ciro23 <ciro.sorre23@gmail.com>
2022-05-13 10:27:53 +0200

    aggiunto metodo signup()

--- app/Controllers/Signup.php
+++ app/Controllers/Signup.php
@@ -11,4 +11,24 @@ class Signup extends BaseController {
     public function index() {
         echo view("Signup", $this->data);
     }
+
+    public function signup() {
+        helper("form");
+
+        if ($this->validate("user")) {
+            $userModel = new \App\Models\User();
+            $userModel->save($this->request->getPost());
+
+            $this->session->set([
+                "is_logged_in" => true,
+                "uid" => $this->request->getPost("tessera_elettorale"),
+            ]);
+
+            return redirect("home");
+        }
+
+        $this->data['errors'] = $this->validator->listErrors("custom_errors");
+
+        echo view("user_auth/signup", $this->data);
+    }
 }
