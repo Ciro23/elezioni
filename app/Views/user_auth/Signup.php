958509dc6c15442b54a43c31254bff65aeab7390
ciro23 <ciro.sorre23@gmail.com>
2022-05-13 10:51:17 +0200

    aggiunti maxlength

--- app/Views/user_auth/Signup.php
+++ app/Views/user_auth/Signup.php
@@ -2,13 +2,13 @@
 
 <form action="/signup-action">
     <label for="tessera-elettorale">Numero tessera elettorale</label>
-    <input type="text" name="tessera-elettorale" id="tessera-elettorale">
+    <input type="text" name="tessera-elettorale" id="tessera-elettorale" maxlength="10">
 
     <label for="nome">Nome</label>
-    <input type="text" name="nome" id="nome">
+    <input type="text" name="nome" id="nome" maxlength="30">
 
     <label for="cognome">Cognome</label>
-    <input type="text" name="cognome" id="cognome">
+    <input type="text" name="cognome" id="cognome" maxlength="30">
 
     <label for="eta">Età</label>
     <input type="number" name="eta" id="eta">
