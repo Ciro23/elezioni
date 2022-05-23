2d57fc8409c58357e84151a18ba0301a91e14c43
ciro23 <ciro.sorre23@gmail.com>
2022-05-23 11:33:55 +0200

    initial upload

---
+++ app/Controllers/EmailVerification.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Controllers;
+
+use CodeIgniter\HTTP\RedirectResponse;
+
+class EmailVerification extends BaseController {
+
+    protected array $data = [
+        "title" => "Verifica il tuo indirizzo email",
+    ];
+
+    public function sendEmail(string $user): void {
+        $emailModel = new \App\Models\Email();
+        $emailModel->sendEmail($user);
+
+        echo view("email_verification/Sending", $this->data);
+    }
+
+    public function verificate(string $hash): RedirectResponse {
+        $emailModel = new \App\Models\Email();
+        if (!$emailModel->verificate($hash)) {
+           return redirect()->route("page-not-found");
+        }
+
+        $emailModel->delete($hash);
+
+        $this->session->set([
+            "is_logged_in" => true,
+            "uid" => $this->request->getPost("tessera_elettorale"),
+        ]);
+
+        return redirect()->to(base_url() . "/votazione/?welcome_popup=true");
+    }
+}
