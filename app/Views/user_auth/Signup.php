117f5e46fec83de321926143f7a01d9d20a27bfb
ciro23 <ciro.sorre23@gmail.com>
2022-05-23 11:35:46 +0200

    modificata route per signup action

--- app/Views/user_auth/Signup.php
+++ app/Views/user_auth/Signup.php
@@ -4,7 +4,7 @@
     <section class="vh-100 gradient-custom">
         <div class="container py-5 h-100 row d-flex justify-content-center align-items-center col-12 col-md-8 col-lg-6 col-xl-5 card bg-light">
             <div class="card-body p-5 text-center">
-                <form class="mb-md-5 mt-md-4 pb-5" action="/signup-action" method="post">
+                <form class="mb-md-5 mt-md-4 pb-5" action="/signup" method="post">
 
                     <h2 class="fw-bold mb-2 text-uppercase" style="color:#008fb3">Registrati</h2>
                     <hr>
@@ -16,28 +16,33 @@
                     ?>
 
                     <div class="form-outline form-white mb-4">
-                        <label class="form-label" for="numScheda">Numero Scheda Elettorale</label>
-                        <input type="text" id="numScheda" name="tessera_elettorale" class="form-control form-control-lg" maxlength="10" placeholder="Inserisci Numero Scheda Elettorale">
+                        <label class="form-label" for="numero_scheda">Numero Scheda Elettorale</label>
+                        <input type="text" id="numero_scheda" name="tessera_elettorale" class="form-control form-control-lg" maxlength="9" placeholder="Inserisci Numero Scheda Elettorale">
                     </div>
 
                     <div class="form-outline form-white mb-4">
-                        <label class="form-label" for="numScheda">Nome</label>
+                        <label class="form-label" for="nome">Nome</label>
                         <input type="text" id="nome" name="nome" class="form-control form-control-lg" maxlength="30" placeholder="Inserisci Nome" />
                     </div>
 
                     <div class="form-outline form-white mb-4">
-                        <label class="form-label" for="numScheda">Cognome</label>
+                        <label class="form-label" for="cognome">Cognome</label>
                         <input type="text" id="cognome" name="cognome" class="form-control form-control-lg" maxlength="30" placeholder="Inserisci Cognome" />
                     </div>
 
                     <div class="form-outline form-white mb-4">
-                        <label class="form-label" for="numScheda">Età</label>
+                        <label class="form-label" for="email">email</label>
+                        <input type="email" id="email" name="email" class="form-control form-control-lg" maxlength="30" placeholder="Inserisci Email" />
+                    </div>
+
+                    <div class="form-outline form-white mb-4">
+                        <label class="form-label" for="eta">Età</label>
                         <input type="number" id="eta" name="eta" class="form-control form-control-lg" placeholder="Inserisci Età" />
                     </div>
 
                     <div class="form-outline form-white mb-4">
-                        <label class="form-label" for="numScheda">Sesso</label>
-                        <select class="form-select form-select-lg" name="sesso" placeholder="Inserisci Sesso">Sesso
+                        <label class="form-label" for="sesso">Sesso</label>
+                        <select class="form-select form-select-lg" id="sesso" name="sesso" placeholder="Inserisci Sesso">Sesso
                             <option value="F">Femmina</option>
                             <option value="M">Maschio</option>
                         </select>
