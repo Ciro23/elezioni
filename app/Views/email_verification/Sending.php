72439ded548f623981009b8a3db551f3855c5cae
ciro23 <ciro.sorre23@gmail.com>
2022-05-24 11:37:26 +0200

    initial upload

---
+++ app/Views/email_verification/Sending.php
@@ -0,0 +1,10 @@
+<?= view_cell("\App\Libraries\ViewCells::header") ?>
+
+<div class="h-50 d-flex justify-content-center align-items-center">
+    <div>
+        <h2>Un'email di conferma è stata inviata al tuo indirizzo</h2>
+        <p>Controlla nella tua casella di posta tramite il soggetto ""</p>
+    </div>
+</div>
+
+<?= view_cell("\App\Libraries\ViewCells::footer") ?>
