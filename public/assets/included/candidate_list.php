d1778c8fd872fa833d0fa37f37fa84f0b319a108
ciro23 <ciro.sorre23@gmail.com>
2022-05-24 11:38:12 +0200

    initial upload

---
+++ public/assets/included/candidate_list.php
@@ -0,0 +1,11 @@
+<option value="">-- nessuno --</option>
+
+<?php
+foreach ($_POST['candidates'] as $candidate) {
+    if (
+        $candidate['partito'] === $_POST['party']
+        && $candidate['id'] != $_POST['already_selected_candidate']
+    ) {
+        echo "<option value='{$candidate['id']}'>{$candidate['nome']}</option>";
+    }
+}
