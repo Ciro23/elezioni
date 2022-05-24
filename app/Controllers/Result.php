41df30284192d1fb8eec32a0641968e9b50f58c9
ciro23 <ciro.sorre23@gmail.com>
2022-05-24 13:34:01 +0200

    initial upload

---
+++ app/Controllers/Result.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Controllers;
+
+class Result extends BaseController {
+
+    protected array $data = [
+        "title" => "Risultati",
+    ];
+
+    public function index() {
+        
+    }
+}
