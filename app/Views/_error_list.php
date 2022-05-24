412b2e9828533d734cbd2388ab9f466075efa40f
ciro23 <ciro.sorre23@gmail.com>
2022-05-24 13:47:29 +0200

    css

--- app/Views/_error_list.php
+++ app/Views/_error_list.php
@@ -1,7 +1,7 @@
 <div>
     <ul>
         <?php foreach ($errors as $error) : ?>
-            <li class="text-red-600 text-center"><?= esc($error) ?></li>
+            <li style="text-decoration: none" class="text-danger"><?= esc($error) ?></li>
         <?php endforeach ?>
     </ul>
 </div>
