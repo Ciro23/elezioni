65d56151ee78f25ae515adf148cbb305679a8280
ciro23 <ciro.sorre23@gmail.com>
2022-05-24 11:35:34 +0200

    la sessione si inizializza entrando col pin

--- app/Controllers/EmailVerification.php
+++ app/Controllers/EmailVerification.php
@@ -13,7 +13,7 @@ class EmailVerification extends BaseController {
 
     public function sendEmail(string $user): void {
         $emailModel = new \App\Models\Email();
-        $emailModel->sendEmail($user);
+        // $emailModel->sendEmail($user);
 
         echo view("email_verification/Sending", $this->data);
     }
@@ -26,11 +26,6 @@ class EmailVerification extends BaseController {
 
         $emailModel->delete($hash);
 
-        $this->session->set([
-            "is_logged_in" => true,
-            "uid" => $this->request->getPost("tessera_elettorale"),
-        ]);
-
-        return redirect()->to(base_url() . "/votazione/?welcome_popup=true");
+        return redirect()->to(base_url() . "/login/?welcome_popup=true");
     }
 }
