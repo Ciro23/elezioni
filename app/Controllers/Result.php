ad233c2e18133d0cadd15c03296b7fd4b0d673cf
ciro23 <ciro.sorre23@gmail.com>
2022-05-31 13:22:41 +0200

    aggiunta view

--- app/Controllers/Result.php
+++ app/Controllers/Result.php
@@ -6,9 +6,18 @@ class Result extends BaseController {
 
     protected array $data = [
         "title" => "Risultati",
+        "votes_for_parties" => [],
+        "votes_for_candidates" => [],
+        "votes_for_gender" => [],
     ];
 
     public function index() {
-        
+        $model = new \App\Models\Vote();
+
+        $this->data['votes_for_parties'] = $model->getVotesForParties();
+        $this->data['votes_for_candidates'] = $model->getVotesForCandidates();
+        $this->data['votes_for_gender'] = $model->getVotesForGender();
+
+        echo view("results/Results", $this->data);
     }
 }
