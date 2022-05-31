a72a6d089f5640e9ac7cfebe78b50d172347485f
ciro23 <ciro.sorre23@gmail.com>
2022-05-31 11:39:53 +0200

    phpdocs

--- app/Models/Vote.php
+++ app/Models/Vote.php
@@ -12,6 +12,11 @@ class Vote extends Model {
 
     protected $validationRules = "vote";
 
+    /**
+     * gets the number of votes of every party
+     * 
+     * @return array
+     */
     public function getVotesForParties(): array {
         $builder = $this->select("partiti.nome, count(voti.id) as n_voti");
         $builder->join("partiti", "partiti.id = voti.partito");
@@ -20,6 +25,11 @@ class Vote extends Model {
         return $builder->get()->getResult();
     }
 
+    /**
+     * gets the number of votes and percentuage for every candidate
+     * 
+     * @return array
+     */
     public function getVotesForCandidates(): array {
         $db = db_connect();
         $result = $db->query("select candidati.nome, candidati.cognome, candidati.partito, (count(*) / (select count(*) from voti) * 50) as percentuale, count(*) as voti from candidati join voti on voti.candidato_1 = candidati.id or voti.candidato_2 = candidati.id group by candidati.id");
