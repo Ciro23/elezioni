0e6dbf1e3f399444b19cfb66a93b59fcd5bb8f34
ciro23 <ciro.sorre23@gmail.com>
2022-05-31 11:37:47 +0200

    aggiunti metodi per ottenere numero dei voti

--- app/Models/Vote.php
+++ app/Models/Vote.php
@@ -12,4 +12,18 @@ class Vote extends Model {
 
     protected $validationRules = "vote";
 
+    public function getVotesForParties(): array {
+        $builder = $this->select("partiti.nome, count(voti.id) as n_voti");
+        $builder->join("partiti", "partiti.id = voti.partito");
+        $builder->groupBy("partito");
+
+        return $builder->get()->getResult();
+    }
+
+    public function getVotesForCandidates(): array {
+        $db = db_connect();
+        $result = $db->query("select candidati.nome, candidati.cognome, candidati.partito, (count(*) / (select count(*) from voti) * 50) as percentuale, count(*) as voti from candidati join voti on voti.candidato_1 = candidati.id or voti.candidato_2 = candidati.id group by candidati.id");
+
+        return $result->getResultArray();
+    }
 }
