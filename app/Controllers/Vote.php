5e2a26218a32910b1e177572fe99836a46cf43c0
ciro23 <ciro.sorre23@gmail.com>
2022-05-31 10:31:05 +0200

    modifiche varie

--- app/Controllers/Vote.php
+++ app/Controllers/Vote.php
@@ -23,18 +23,27 @@ class Vote extends BaseController {
         
         if ($this->validate("vote")) {
             $formData = $this->request->getPost();
+            
+            if ($formData['candidato_1'] === "") {
+                $formData['candidato_1'] = null;
+            }
+
+            if ($formData['candidato_2'] === "") {
+                $formData['candidato_2'] = null;
+            }
+
             $voteModel->save($formData);
+            $userModel = new \App\Models\User();
+
+            $electoralCardNumber = $userModel->getUserElectoralCardByPin($this->session->pin);
+            $userModel->update($electoralCardNumber, ['ha_votato' => 1]);
 
             $this->session->destroy();
 
-            return redirect("risultati");
+            return redirect("results");
         }
 
-        $model = new \App\Models\Party();
-        $this->data['parties'] = $model->getAllParties();
-        $this->data['candidates'] = $model->getAllCandidates();
         $this->data['errors'] = $this->validator->listErrors("custom_errors");
-
         echo view("voting/Vote", $this->data);
     }
 }
